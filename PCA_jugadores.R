8fe9b8dae9fcf71734f3f5c26c028af32271a092
pmtempone <pmtempone@gmail.com>
2016-08-24 11:16:04 -0300

    PCA

--- PCA_jugadores.R
+++ PCA_jugadores.R
@@ -7,5 +7,23 @@ library(ggplot2)
 ----#Analisis de componentes principales con jugadores----
 
 pca_jugadores <- PCA(matjugadores,scale.unit=TRUE, ncp=5, graph=T)
+ellipse.coord = coord.ellipse(concat,bary=T)
+plot.PCA(pca_jugadores, axes=c(1, 2), choix="ind")
 
-plot.PCA(pca_jugadores, axes=c(1, 2), choix="ind", habillage=2)
+dimdesc(pca_jugadores, axes=c(1,2))
+
+
+----#HCLUST-----
+
+res.hcpc = HCPC(pca_jugadores)
+
+res.hcpc
+
+res.hcpc$desc.var$quanti.var
+
+----#cantidad de PC-----
+
+FactoMineR::estim_ncp(matjugadores) #3 componentes
+
+
+FactoMineR::plot.HCPC(res.hcpc)
