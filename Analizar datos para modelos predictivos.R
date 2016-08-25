01d70e48224c93beb1c71e3ac5c9d96cf5e8cfa4
pmtempone <pmtempone@gmail.com>
2016-08-25 00:18:10 -0300

    v24-08

--- Analizar datos para modelos predictivos.R
+++ Analizar datos para modelos predictivos.R
@@ -17,4 +17,36 @@ Basetotal=Basetotal[, !(names(Basetotal) %in% vars_to_remove[,"variable"])]
 
 #Deteccion de outliers
 
+summary(Basetotal)
+
+# checking the value for the top 1% of highest values (percentile 0.99), which is ~ 7.05
+quantile(Basetotal$pase_correcto, 0.99)
+
+
+----#selecting variables----
+
+p <- ggplot(Basetotal) + geom_boxplot(aes(x=titular, y=minutos_jugados, color=titular))
+
+p
+
+----#cross plot-----
+
+cross_minutos=cross_plot(Basetotal, str_input="minutos_jugados", str_target="titular")
+
+vars_to_analyze=c("torneo", "team.1", "rol_id_rol","pase_correcto")
+
+cross_plot(data=Basetotal, str_target="titular", str_input=vars_to_analyze)
+
+----#boxplot----
+
+plotar(data=Basetotal, str_input="minutos_jugados", str_target="titular", plot_type = "boxplot")
+
+ggplot(data = Basetotal,mapping = aes(x=rol_id_rol,y=minutos_jugados,col=rol_id_rol))+geom_boxplot()
+
+
+----#density plot----
+
+plotar(data=Basetotal, str_input="minutos_jugados", str_target="titular", plot_type = "histdens")
+
+plotar(data=Basetotal, str_input="pase_correcto", str_target="titular", plot_type = "histdens")
 
