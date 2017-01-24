d67c6cd3148ebb20ce764c407deb27590eab12a3
pmtempone <pmtempone@gmail.com>
2017-01-24 00:40:08 -0300

    diferencia de medias - anova

--- 06 - anova en tiros al arco.R
+++ 06 - anova en tiros al arco.R
@@ -18,11 +18,20 @@ library(dummies)
 library(ggcorrplot)
 library(fmsb)
 library(reshape2)
+library(readxl)   #Para leer los archivos de excel
+library(psych)    #Para calcular las estadísticas por grupo
+library(nortest)  #Para hacer las pruebas de hipótesis de normalidad
+library(lawstat)  #Prueba de Levene
+library(reshape)  #Manejo de datos
+library(foreign)
+library(dplyr)
+
+library(Rcmdr)
 options(scipen = 999)
 
 ----#crear dataset con tiros de titulares y suplentes---
   
-jugadores_tiros <- jugadores_var %>% dplyr::select(jugador,J_local,pr_disparo_afuera)
+jugadores_tiros <- jugadores_var %>% dplyr::select(jugador,J_local,pr_disparo_afuera) %>% dplyr::filter()
 
 jugadores_tiros <- acast(jugadores_tiros, jugador~J_local, value.var="pr_disparo_afuera")
 
@@ -34,63 +43,53 @@ jugadores_tiros <- jugadores_tiros[complete.cases(jugadores_tiros),]
 ggplot() + geom_density(aes(x=L), colour="red", data=jugadores_tiros) + 
   geom_density(aes(x=V), colour="blue", data=jugadores_tiros)
 
-
+jugadores_grafico <- jugadores_var %>% dplyr::select(jugador,J_local,pr_disparo_afuera)
 
 ggplot(jugadores_var %>% dplyr::select(jugador,J_local,pr_disparo_afuera),aes(x=pr_disparo_afuera, fill=J_local)) + geom_density(alpha=0.25)
+
+ggplot(data=jugadores_grafico,aes(x=J_local,y=pr_disparo_afuera,fill=J_local)) + geom_boxplot()
 
 
 ----#Hipotesis-----
 
 'Hipótesis nula:
-Los promedios de las actuaciones de los jugadores son iguales tanto de visitante como de local.
+Los promedios de las tiros al arco de afuera de los jugadores son iguales tanto de visitante como de local.
 H0:μ1=μ2=⋯=μn=μ
 Hipótesis alternativa:
-Existe al menos un promedio de las actuaciones de los jugadores tanto de visitante como de local es diferente de los demás.
+Existe al menos un promedio de las  tiros al arco de afuera de los jugadores tanto de visitante como de local es diferente de los demás.
 Ha:∃μi≠μ
 , i=1,…,n
 Análisis discriminante:'
 
-Y <- jugadores_var %>% dplyr::select(J_local,pr_goles_convertidos:pr_atajadas)
+describeBy(jugadores_grafico$pr_disparo_afuera, group = jugadores_grafico$J_local, mat = T)[,c("group1", "mean", "sd")]
 
-Y$perso_nombre.1 <- NULL
-Y$perso_apellido.1 <- NULL
+jugadores.AOV = aov(data = jugadores_grafico, pr_disparo_afuera ~ J_local)
+jugadores.AOV
+summary(jugadores.AOV)
 
-Y$J_local <- factor(Y$J_local)
+#b- Realice la prueba y el análisis diagnostico(supuestos)
+jugadores_dif_medias = cbind.data.frame(jugadores_grafico, residuals(jugadores.AOV))
+colnames(jugadores_dif_medias)[4] = "Anova.Residuales"
 
-describeBy(Y[,2:15], group = Y$J_local, mat = T)[,c("group1", "mean", "sd")]
 
-#melt para hacer multiples boxplots
+Anderson <- ad.test(jugadores_dif_medias$Anova.Residuales)
+Kolmogorov <- lillie.test(jugadores_dif_medias$Anova.Residuales)
+ShapiroWilk <- shapiro.test(jugadores_dif_medias$Anova.Residuales)
+Cramer <- cvm.test(jugadores_dif_medias$Anova.Residuales)
 
-Y_v2 <- as.data.frame(jugadores_var[,c(2,22:35)])
+rbind(cbind(Anderson$method,'p valor' = Anderson$p.value),
+      cbind(Kolmogorov$method,'p valor' = Kolmogorov$p.value),
+      cbind(ShapiroWilk$method,'p valor' = ShapiroWilk$p.value),
+      cbind(Cramer$method,'p valor' = Cramer$p.value))
 
-df.m <- melt(Y_v2,id.vars = "J_local")
+#Ninguna de las pruebas de normalidad rechaza la hipótesis nula, es decir, se puede asumir que los residuales del modelo tienen distribución normal.
 
-ggplot(data = df.m, aes(x = variable, y = value)) + geom_boxplot(aes(fill=J_local))
+#Pruebas de homocedasticidad:
 
-----#test de hotelling-----
-(m1 <- with(Y_v2, HotellingsT2(cbind(pr_goles_convertidos,pr_asistencias,pr_disparo_afuera,
-                                     pr_disparo_atajado,pr_faltas,pr_faltas_recibidos,pr_offsides,
-                                     pr_amarillas,pr_expulsados,pr_pase_correcto,pr_incorrecto,
-                                     pr_despejes,pr_quites,pr_atajadas) ~ J_local)))
+Levene <- leveneTest(jugadores_dif_medias$Anova.Residuales, jugadores_dif_medias$J_local)
+Bartlett <- bartlett.test(jugadores_dif_medias$Anova.Residuales, jugadores_dif_medias$J_local)
 
-#se rechaza la hipotesis nula, no se comportan de igual manera los jugadores de local y visitante
+rbind(cbind("Levene",'p valor' = Levene$`Pr(>F)`),
+      cbind("Bartlett",'p valor' = Bartlett$p.value))
 
-y_mat <- as.matrix(Y)
-
-fit = hotelling.test(.~J_local, Y, perm = TRUE)
-fit
-plot(fit)
-plot(fit, col = "lightblue")
-
-#Normalidad multivariada (Se usa el test de Royston):'
-
-roystonTest(Y_v2[, -1])
-
-m2 <- manova(cbind(pr_goles_convertidos,pr_asistencias,pr_disparo_afuera,
-                   pr_disparo_atajado,pr_faltas,pr_faltas_recibidos,pr_offsides,
-                   pr_amarillas,pr_expulsados,pr_pase_correcto,pr_incorrecto,
-                   pr_despejes,pr_quites,pr_atajadas) ~ J_local,Y_v2)
-
-summary(m2) #no son iguales las varianzas
-
-#No se satisface el supuesto de normalidad multivariada, por lo tanto no es posible hacer un análisis discriminante a menos que se encuentre una transformación que permita normalizar los datos.
+#Se satisface la hipótesis de homocedasticidad entre los grupos.
