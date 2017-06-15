cae02cd6d4098d2d7b6ff81336951097079518cf
pmtempone <pmtempone@gmail.com>
2017-06-15 16:53:28 -0300

    creacion de variables

--- 12 generar los modelos.R
+++ 12 generar los modelos.R
@@ -1,9 +1,35 @@
 #modelo inicial
 
+
 library(rpart)
 library(rpart.plot)
+library(party)
+library(caret)
+library(rattle)
+library(pROC)
+library(rpart.utils)
+library(randomForest)
+library(funModeling)
+library(dplyr)
+library(foreach)
+library(arules)
+
+train_index <- caret::createDataPartition(base_modelado_completa$resultado_local,p=0.7)
+
+train_completa <- base_modelado_completa[train_index$Resample1,]
+test_completa <- base_modelado_completa[-train_index$Resample1,]
 
 
-fit.rpart <- rpart(resultado_local ~ ., data = base_modelado_completa[,c(2:31,34)])
+fit.rpart <- rpart(resultado_local ~ ., data = train_completa[,c(2:31,34)])
 
 rpart.plot(fit.rpart)
+
+pred.rpart <- predict(fit.rpart,test_completa)
+
+pred.rpart.corte <- ifelse(pred.rpart>0.2,1,0)
+
+table(predicho=pred.rpart.corte,observador=test_completa$resultado_local)
+
+g <- roc(resultado_local ~ pred.rpart.corte, data = test_completa)
+plot(g, col="red")
+g
--- base_avg_equipos_eventos.sql
+++ base_avg_equipos_eventos.sql
@@ -19,3 +19,71 @@ FROM   base_evento_equipo
 WINDOW w AS (PARTITION BY equipo
              ORDER BY base_evento_equipo
              ROWS BETWEEN 6 PRECEDING AND 1 PRECEDING);
+             
+--base sum de eventos equipos
+
+SELECT even_id_evento, equipo,sum(goles) OVER w as sum_goles,
+sum(asistencias) OVER w as sum_asistencias,
+sum(disp_afuera) OVER w as sum_disp_afuera,
+sum(disp_palo) OVER w as sum_disp_palo,
+sum(disp_atajado) OVER w as sum_disp_atajado,
+sum(penal_errado) OVER w as sum_penal_errado,
+sum(faltas) OVER w as sum_faltas,
+sum(offsides) OVER w as sum_offsides,
+sum(amarillas) OVER w as sum_amarillas,
+sum(doble_amarilla) OVER w as sum_doble_amarilla,
+sum(despejes) OVER w as sum_despejes,
+sum(quites) OVER w as sum_quites,
+sum(atajadas) OVER w as sum_atajadas,
+sum(ataj_penal) OVER w as sum_ataj_penal
+into base_sum_equipos_eventos
+FROM   base_evento_equipo
+WINDOW w AS (PARTITION BY equipo
+             ORDER BY base_evento_equipo
+             ROWS BETWEEN 6 PRECEDING AND 1 PRECEDING);
+             
+--base max de eventos equipos
+
+SELECT even_id_evento, equipo,max(goles) OVER w as max_goles,
+max(asistencias) OVER w as max_asistencias,
+max(disp_afuera) OVER w as max_disp_afuera,
+max(disp_palo) OVER w as max_disp_palo,
+max(disp_atajado) OVER w as max_disp_atajado,
+max(penal_errado) OVER w as max_penal_errado,
+max(faltas) OVER w as max_faltas,
+max(offsides) OVER w as max_offsides,
+max(amarillas) OVER w as max_amarillas,
+max(doble_amarilla) OVER w as max_doble_amarilla,
+max(despejes) OVER w as max_despejes,
+max(quites) OVER w as max_quites,
+max(atajadas) OVER w as max_atajadas,
+max(ataj_penal) OVER w as max_ataj_penal
+into base_max_equipos_eventos
+FROM   base_evento_equipo
+WINDOW w AS (PARTITION BY equipo
+             ORDER BY base_evento_equipo
+             ROWS BETWEEN 6 PRECEDING AND 1 PRECEDING);
+             
+--base min de eventos equipos
+
+SELECT even_id_evento, equipo,min(goles) OVER w as min_goles,
+min(asistencias) OVER w as min_asistencias,
+min(disp_afuera) OVER w as min_disp_afuera,
+min(disp_palo) OVER w as min_disp_palo,
+min(disp_atajado) OVER w as min_disp_atajado,
+min(penal_errado) OVER w as min_penal_errado,
+min(faltas) OVER w as min_faltas,
+min(offsides) OVER w as min_offsides,
+min(amarillas) OVER w as min_amarillas,
+min(doble_amarilla) OVER w as min_doble_amarilla,
+min(despejes) OVER w as min_despejes,
+min(quites) OVER w as min_quites,
+min(atajadas) OVER w as min_atajadas,
+min(ataj_penal) OVER w as min_ataj_penal
+into base_min_equipos_eventos
+FROM   base_evento_equipo
+WINDOW w AS (PARTITION BY equipo
+             ORDER BY base_evento_equipo
+             ROWS BETWEEN 6 PRECEDING AND 1 PRECEDING);
+             
+             
